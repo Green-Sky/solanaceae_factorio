fcafb1bd7e6038ba268dd39b67d9593a48c6c224
Green Sky <green@g-s.xyz>
2024-06-10 22:27:19 +0200

    add filewatch

--- src/factorio.cpp
+++ src/factorio.cpp
@@ -5,7 +5,13 @@
 
 #include "./log_parse.hpp"
 
-Factorio::Factorio(Contact3Registry& cr, RegistryMessageModel& rmm) : _cr(cr), _rmm(rmm) {
+Factorio::Factorio(Contact3Registry& cr, RegistryMessageModel& rmm) :
+	_cr(cr),
+	_rmm(rmm),
+	_fw("test.txt", [this](const auto& path, const auto event){ this->onFileEvent(path, event);})
+{
+
+
 	_rmm.subscribe(this, RegistryMessageModel_Event::message_construct);
 }
 
@@ -14,5 +20,9 @@ Factorio::~Factorio(void) {
 
 bool Factorio::onEvent(const Message::Events::MessageConstruct& e) {
 	return false;
+}
+
+void onFileEvent(const std::string& path, const filewatch::Event change_type) {
+	std::cout << "file even " << filewatch::event_to_string(change_type) << " on '" << path << "'\n";
 }
 
